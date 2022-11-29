dfffc60f506697b45831c82a60ff10615870b960
Anna Peterson <reveragb@gmail.com>
2022-11-29 14:50:48 -0700

    Add game class

--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,6 @@
+const Card = require('./Card');
+const Deck = require('./Deck');
+const Round = require('./Round');
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
@@ -13,6 +16,18 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
+
+  start() {
+    var cards = prototypeQuestions.map(question => {
+      return new Card(question.id, question.question, question.answers, question.correctAnswer)
+    })
+    var deck = new Deck(cards)
+    var round = new Round(deck)
+    this.currentRound = round
+    this.printMessage(deck, round)
+    this.printQuestion(round)
+  }
+
 }
 
 module.exports = Game;
